512425219121f4accbb963f3d7a45c33b949d09a
shawn_its <psh2968@naver.com>
2023-10-31 05:46:02 +0900

    수정

--- medi/src/pages/Pills.jsx
+++ medi/src/pages/Pills.jsx
@@ -1,111 +1,132 @@
 import React, { useState, useEffect } from 'react';
 import colors from "../styles/colors";
 import styled from "styled-components";
+import searchkey from "../assets/images/searchkey.png";
 import Banner1 from "../assets/images/Banner1.png";
 import Banner2 from "../assets/images/Banner2.png";
 import Banner3 from "../assets/images/Banner3.png";
 import buttonLeft from "../assets/images/ButtonLeft.png";
 import buttonRight from "../assets/images/ButtonRight.png";
 
+
 const PillsContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
+`
 
+//검색과 필터링 박스
 const First= styled.div`
-  width: 54.5vw;
-  margin-top: 2vw;
   display: flex;
-  justify-content: center;
-`;
-
-const PillsSearch = styled.div`
-  display: flex;
-  justify-content: space-between;
   align-items: center;
-  width: 100%;
-  color: ${colors.lightgray};
-`;
+  margin-top: 5rem;
+  gap:2vw;
+`
 
+//검색
+const PillsSearch = styled.div`
+  width: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${colors.lightgray};
+`
 const SearchInput = styled.input`
   width: 25vw;
   height: 3vw;
-  font-size: 1vw;
+  font-size: 2rem;
   border: 0.2vw solid #2A2A3A;
   border-radius: 1.5vw;
   background: #191B24;
-  color: ${colors.white};
+  color: #484A64;
   outline: none;
-  padding: 0.5vw 2vw 0.5vw 2vw;
-`;
+  padding: 1.5vw 3.5vw 1.5vw 3vw;
+`
+const SearchImage = styled.img`
+  width: 1.5vw;
+  height: 1.5vw;
+  position: absolute;
+  margin-top: 0vw;
+  margin-right: 21vw;
+  z-index: 1;
+`
+
+// //필터링
+// const SelectContainer = styled.div`
+//   display: flex;
+// `;
 
 const Select = styled.select`
   width: 100% 
   align-items: center;
   justify-content: center;
-  font-size: 1vw;
+  font-size: 2rem;
   padding: 0.5vw 2vw 0.5vw 2vw;
   height: 3vw;
   border: 0.2vw solid #2A2A3A;
   border-radius: 1.5vw;
   background: #191B24;
   color: #484A64;
-`;
+`
 
-//배너
+//배너 슬라이드
 const Slides = styled.div`
   width: 100%;
   height: 100%;
   border: none;
   border-radius: 0.4vw;
-`;
-
+`
+//배너
 const Banner = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100vw;
-`;
+`
 
 const PillsBannerContainer = styled.div`
-  width: 59vw;
-  height: 8.5vw;
+  width: 50vw;
+  height: 6.75vw;
   border-radius: 0.4vw;
   display: flex;
   justify-content: center;
+  text-align: center;
   align-items: center;
   margin-top: 2.6vw;
   margin-bottom: 1rem;
-  gap: 1vw;
-`;
-
+`
+//배너 이미지
 const StyledImage = styled.img`
-  width: 100%;
-  height: 100%;
+  max-width: 100%;
+  max-height: 200%;
   border-radius: 0.4vw;
-`;
-
-//양쪽 버튼
+`
+//배너 양쪽 버튼
 const Button = styled.img`
   width: 1.2vw;
   height: 1.2vw;
   cursor: pointer;
-`;
+`
 
 const ButtonLeftStyled = styled(Button)`
-`;
+  position: relative;
+  top: 30%;
+  left: -10%;
+`
 
 const ButtonRightStyled = styled(Button)`
-`;
+  position: relative;
+  top: 30%;
+  right: -10%;
+`
 
 //배너 밑 동그라미 3개
 const CircleContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 0.5vw;
-`;
+  margin-top: 20rem;
+`
 
 const Circle = styled.div`
   width: 0.5vw;
@@ -120,14 +141,14 @@ const Circle = styled.div`
   &.active {
     background: #484A64;
   }
-`;
+`
 
 
-const Pills = () => {
+
+function Pills() {
 
   //필터링
-  const OPTIONS = [
-    { value: "age", name: "나이" },
+  const OPTIONS1 = [
     { value: "child", name: "어린이" },
     { value: "Teenager", name: "청소년" },
     { value: "adult", name: "성인" },
@@ -135,8 +156,7 @@ const Pills = () => {
     { value: "pregnant", name: "임산부" },
   ];
 
-  const OPTIONSS = [
-    { value: "Usage", name: "용도" },
+  const OPTIONS2 = [
     { value: "tired", name: "피로회복" },
     { value: "eye", name: "눈 건강" },
     { value: "bone", name: "뼈 건강" },
@@ -146,17 +166,18 @@ const Pills = () => {
   ];
 
 
-  const SelectBox = ({ options, defaultValue, onChange }) => {
-    return (
-      <Select defaultValue={defaultValue} onChange={onChange}>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.name}
-          </option>
-        ))}
-      </Select>
-    );
+  const [selectedAgeOption, setSelectedAgeOption] = useState('');
+  const [selectedUsageOption, setSelectedUsageOption] = useState('');
+
+  const handleAgeOptionChange = (event) => {
+    setSelectedAgeOption(event.target.value);
   };
+
+  const handleUsageOptionChange = (event) => {
+    setSelectedUsageOption(event.target.value);
+  };
+
+
 
 
   //배너
@@ -166,11 +187,8 @@ const Pills = () => {
   const handleSlideChange = (direction) => {
     let newIndex = currentSlide + direction;
 
-    if (newIndex < 0) {
-      newIndex = images.length - 1;
-    } else if (newIndex >= images.length) {
-      newIndex = 0;
-    }
+    if (newIndex < 0) {newIndex = images.length - 1;} 
+      else if (newIndex >= images.length) {newIndex = 0;}
 
     setCurrentSlide(newIndex);
   }
@@ -179,18 +197,12 @@ const Pills = () => {
     const autoSlideTimer = setInterval(() => {
       setCurrentSlide((prevSlide) => {
         let newIndex = prevSlide + 1;
-
-        if (newIndex >= images.length) {
-          newIndex = 0;
-        }
-
+        if (newIndex >= images.length) {newIndex = 0;}
         return newIndex;
       });
     }, 3000);
 
-    return () => {
-      clearInterval(autoSlideTimer);
-    };
+    return () => {clearInterval(autoSlideTimer);};
   }, [images.length]);
 
 
@@ -199,11 +211,26 @@ const Pills = () => {
   return (
     <PillsContainer>
       <First>
-      <PillsSearch>
-        <SearchInput type="search" placeholder="약품을 검색하세요." />
-        <SelectBox options={OPTIONS}> </SelectBox> 
-        <SelectBox options={OPTIONSS}> </SelectBox> 
-      </PillsSearch>
+        <PillsSearch> 
+          <SearchImage id="icon_search" src={searchkey} alt="searchkey" />
+          <SearchInput type="search" placeholder="약품을 검색하세요." />
+        </PillsSearch>
+        <Select value={selectedAgeOption} onChange={handleAgeOptionChange}>
+          <option value="">나이</option>
+          {OPTIONS1.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.name}
+            </option>
+          ))}
+        </Select>
+        <Select value={selectedUsageOption} onChange={handleUsageOptionChange}>
+          <option value="">용도</option>
+          {OPTIONS2.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.name}
+            </option>
+          ))}
+        </Select>
       </First>
 
       <Banner>
