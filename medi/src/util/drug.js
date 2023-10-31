bf9242a520c51f6428f853ac491c506d75d55d00
sheepyis <zx9086@naver.com>
2023-10-31 13:14:37 +0900

    drugData 1차 정리

--- medi/src/util/drug.js
+++ medi/src/util/drug.js
@@ -5,7 +5,13 @@ const DrugData = [
         "type": "비타민A",
         "name": "드라이 비타민A",
         "image": "images/drug-1.png",
-        "explain": "비타민 A는 눈의 망막 색소의 구성성분으로서, 눈의 기능을 도와 시력을 유지시키는 역할을 합니다. 그 외에 도 세포의 분화와 성장, 뼈와 치아 발달, 장기의 형성과 기능 발휘, 피부나 점막 등 상피조직의 유지에도 관련이 있습니다."
+        "explain": "약 설명", 
+
+        "modalExplain": "비타민 A는 눈의 망막 색소의 구성성분으로서, 눈의 기능을 도와 시력을 유지시키는 역할을 합니다. 그 외에 도 세포의 분화와 성장, 뼈와 치아 발달, 장기의 형성과 기능 발휘, 피부나 점막 등 상피조직의 유지에도 관련이 있습니다. 식품으로는 간, 계란 노른자, 버터, 우유 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-liver.png",
+        "modalImage2": "images/food-egg.png",
+        "modalImage3": "images/food-butter.png",
+        "modalImage4": "images/food-milk.png"
     },
     {
         "id": 2,
@@ -13,7 +19,13 @@ const DrugData = [
         "type": "비타민A",
         "name": "비타민A 츄어블",
         "image": "images/drug-2.png",
-        "explain": "비타민 A는 눈의 망막 색소의 구성성분으로서, 눈의 기능을 도와 시력을 유지시키는 역할을 합니다. 그 외에 도 세포의 분화와 성장, 뼈와 치아 발달, 장기의 형성과 기능 발휘, 피부나 점막 등 상피조직의 유지에도 관련이 있습니다."
+        "explain": "약 설명", 
+
+        "modalExplain": "비타민 A는 눈의 망막 색소의 구성성분으로서, 눈의 기능을 도와 시력을 유지시키는 역할을 합니다. 그 외에 도 세포의 분화와 성장, 뼈와 치아 발달, 장기의 형성과 기능 발휘, 피부나 점막 등 상피조직의 유지에도 관련이 있습니다. 식품으로는 간, 계란 노른자, 버터, 우유 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-liver.png",
+        "modalImage2": "images/food-egg.png",
+        "modalImage3": "images/food-butter.png",
+        "modalImage4": "images/food-milk.png"
     },
     {
         "id": 3,
@@ -21,7 +33,13 @@ const DrugData = [
         "type": "비타민B1",
         "name": "비타민 B1",
         "image": "images/drug-3.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "비타민 B1은 체내 에너지 생산에 중요한 역할을 하는 영양소입니다. 효능으로는 인지 건강, 운동능력 증진, 체중 조절, 심장 건강 등이 있습니다. 식품으로는 싱싱한 고기, 계란 노른자, 우유, 곡식, 채소, 과일 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-meat.png",
+        "modalImage2": "images/food-egg.png",
+        "modalImage3": "images/food-milk.png",
+        "modalImage4": "images/food-fruits.png"
     },
     {
         "id": 4,
@@ -29,15 +47,24 @@ const DrugData = [
         "type": "비타민B1",
         "name": "Vitamin B1",
         "image": "images/drug-4.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "비타민 B1은 체내 에너지 생산에 중요한 역할을 하는 영양소입니다. 효능으로는 인지 건강, 운동능력 증진, 체중 조절, 심장 건강 등이 있습니다. 식품으로는 싱싱한 고기, 계란 노른자, 우유, 곡식, 채소, 과일 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-meat.png",
+        "modalImage2": "images/food-egg.png",
+        "modalImage3": "images/food-milk.png",
+        "modalImage4": "images/food-fruits.png"
     },
     {
+        //식품 내용, 사진 필요
         "id": 5,
         "company": "뉴트리메이드",
         "type": "아연",
         "name": "아연",
         "image": "images/drug-5.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "아연은 여러가지 생리적 기능을 조절하고 세포의 생존 및 성장에 필수적인 역할을 합니다. 근육의 탄력성 및 회복력을 향상시키는 데에 도움을 주며, 근육 생리학적으로 중요한 아미노산 중 하나인 아르지닌은 아연에 의해 상당히 규정되기 때문에 근육 분해 및 회복 속도를 조절하는 데에도 영향을 줍니다. 또한 혈중 트림에토판 수치를 높이고, 생체에서 단백질의 합성을 촉진시켜 근관환 요구를 충족시킵니다. 이러한 과정에서 근육 손상이 최소화되고, 근육 성장이 증진되며, 근력의 유지 능력이 향상됩니다. 마지막으로, 아연은 성장기 아동들의 조직 및 뼈 건강에도 중요한 영향을 미칩니다. 세포 분열과 성장을 조절하여 성장하는 아이들에게 필요한 영양성분을 제공합니다."
     },
     {
         "id": 6,
@@ -45,23 +72,37 @@ const DrugData = [
         "type": "아연",
         "name": "바른 아연",
         "image": "images/drug-6.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "아연은 여러가지 생리적 기능을 조절하고 세포의 생존 및 성장에 필수적인 역할을 합니다. 근육의 탄력성 및 회복력을 향상시키는 데에 도움을 주며, 근육 생리학적으로 중요한 아미노산 중 하나인 아르지닌은 아연에 의해 상당히 규정되기 때문에 근육 분해 및 회복 속도를 조절하는 데에도 영향을 줍니다. 또한 혈중 트림에토판 수치를 높이고, 생체에서 단백질의 합성을 촉진시켜 근관환 요구를 충족시킵니다. 이러한 과정에서 근육 손상이 최소화되고, 근육 성장이 증진되며, 근력의 유지 능력이 향상됩니다. 마지막으로, 아연은 성장기 아동들의 조직 및 뼈 건강에도 중요한 영향을 미칩니다. 세포 분열과 성장을 조절하여 성장하는 아이들에게 필요한 영양성분을 제공합니다."
     },
     {
         "id": 7,
         "company": "Doctor's Best",
-        "type": "비타민D3",
+        "type": "비타민D",
         "name": "비타민 D3",
         "image": "images/drug-7.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "비타민 D는 신체가 칼슘을 흡수하도록 도와주기 때문에 건강한 뼈, 근육 및 치아에 중요한 영양소입니다. 호르몬 인슐린과 글루카곤의 적절한 기능을 도와 체내에서의 적절한 포도당 흡수를 유지할 때 도움을 주어 당뇨병 위험을 감소시킵니다. 또한 신체의 면역력을 높여 암, 관절염, 파킨슨병 등 퇴행성 질환의 위험을 감소시킵니다. 우을증과 기분 변화를 유발할 수 있어 기분을 크게 향상시키고, 정신 검강을 증진 시킬 수 있습니다. 체중 감량에 도움을 주는 세로토닌과 테스토스테론의 수치를 증가시키기 때문에 지방 세포의 저장과 형성을 변화시켜 체중 감소를 촉진합니다. 식품으로는 간, 계란 노른자, 고등어 등 기름기 있는 생선을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-liver.png",
+        "modalImage2": "images/food-egg.png",
+        "modalImage3": "images/food-fish.png",
+        "modalImage4": "images/food-sun.png"
     }, 
     {
         "id": 8,
         "company": "뉴트리코어",
-        "type": "비타민D3",
+        "type": "비타민D",
         "name": "비타민 D",
         "image": "images/drug-8.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "비타민 D는 신체가 칼슘을 흡수하도록 도와주기 때문에 건강한 뼈, 근육 및 치아에 중요한 영양소입니다. 호르몬 인슐린과 글루카곤의 적절한 기능을 도와 체내에서의 적절한 포도당 흡수를 유지할 때 도움을 주어 당뇨병 위험을 감소시킵니다. 또한 신체의 면역력을 높여 암, 관절염, 파킨슨병 등 퇴행성 질환의 위험을 감소시킵니다. 우을증과 기분 변화를 유발할 수 있어 기분을 크게 향상시키고, 정신 검강을 증진 시킬 수 있습니다. 체중 감량에 도움을 주는 세로토닌과 테스토스테론의 수치를 증가시키기 때문에 지방 세포의 저장과 형성을 변화시켜 체중 감소를 촉진합니다. 식품으로는 간, 계란 노른자, 고등어 등 기름기 있는 생선을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-liver.png",
+        "modalImage2": "images/food-egg.png",
+        "modalImage3": "images/food-fish.png",
+        "modalImage4": "images/food-sun.png"
     },
     {
         "id": 9,
@@ -69,7 +110,13 @@ const DrugData = [
         "type": "비타민C",
         "name": "비타민C 구미젤리",
         "image": "images/drug-9.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "비타민 C는 수용성 비타민의 한 종류입니다. 귤과 같은 과일이나 여러 채소에 풍부하게 들어있고 정제하여 건강 보조 식품으로 판매되기도 하고, 괴혈병의 예방 및 치료제로 쓰이기도 합니다. 식품으로는 귤, 키위, 감자, 채소 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-orange.png",
+        "modalImage2": "images/food-kiwi.png",
+        "modalImage3": "images/food-potato.png",
+        "modalImage4": "images/food-vegetable.png"
     },
     {
         "id": 10,
@@ -77,7 +124,13 @@ const DrugData = [
         "type": "비타민C",
         "name": "비타민C 1000",
         "image": "images/drug-10.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "비타민 C는 수용성 비타민의 한 종류입니다. 귤과 같은 과일이나 여러 채소에 풍부하게 들어있고 정제하여 건강 보조 식품으로 판매되기도 하고, 괴혈병의 예방 및 치료제로 쓰이기도 합니다. 식품으로는 귤, 키위, 감자, 채소 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-orange.png",
+        "modalImage2": "images/food-kiwi.png",
+        "modalImage3": "images/food-potato.png",
+        "modalImage4": "images/food-vegetable.png"
     },
     {
         "id": 11,
@@ -85,7 +138,13 @@ const DrugData = [
         "type": "루테인",
         "name": "루테인",
         "image": "images/drug-11.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "루테인은 외부 공격으로 눈을 보호하고 회복시켜 눈 건강에 도움을 줍니다. 몸에 누적되는 항산화 성분으로 피부에도 존재하여 먹는 선크림 역할을 하기 때문에 자외선과 외부 스트레스로부터 피부를 보호해 건강에 도움을 줍니다. 황반 색소 밀도를 늘려 뇌 기능 개선에도 도움을 주며, 특히 뇌를 구성하는 백질에 긍정적인 영향을 줍니다. 식품으로는 시금치, 케일, 브로콜리, 노란 고추, 각종 야채와 과일 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-spinach.png",
+        "modalImage2": "images/food-kale.png",
+        "modalImage3": "images/food-broccoli.png",
+        "modalImage4": "images/food-fruits.png"
     },
     {
         "id": 12,
@@ -93,15 +152,28 @@ const DrugData = [
         "type": "루테인",
         "name": "루테인아스타잔틴+",
         "image": "images/drug-12.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "루테인은 외부 공격으로 눈을 보호하고 회복시켜 눈 건강에 도움을 줍니다. 몸에 누적되는 항산화 성분으로 피부에도 존재하여 먹는 선크림 역할을 하기 때문에 자외선과 외부 스트레스로부터 피부를 보호해 건강에 도움을 줍니다. 황반 색소 밀도를 늘려 뇌 기능 개선에도 도움을 주며, 특히 뇌를 구성하는 백질에 긍정적인 영향을 줍니다. 식품으로는 시금치, 케일, 브로콜리, 노란 고추, 각종 야채와 과일 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-spinach.png",
+        "modalImage2": "images/food-kale.png",
+        "modalImage3": "images/food-broccoli.png",
+        "modalImage4": "images/food-fruits.png"
     },
     {
+        //노션 정리 필요
         "id": 13,
         "company": "NaturesPlus",
         "type": "칼슘",
         "name": "칼슘",
         "image": "images/drug-13.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "식품으로는 간, 계란노른자, 버터, 우류, 치즈 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-liver.png",
+        "modalImage2": "images/food-egg.png",
+        "modalImage3": "images/food-butter.png",
+        "modalImage4": "images/food-milk.png"
     },
     {
         "id": 14,
@@ -109,15 +181,28 @@ const DrugData = [
         "type": "칼슘",
         "name": "칼슘",
         "image": "images/drug-14.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "식품으로는 간, 계란노른자, 버터, 우류, 치즈 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-liver.png",
+        "modalImage2": "images/food-egg.png",
+        "modalImage3": "images/food-butter.png",
+        "modalImage4": "images/food-milk.png"
     },
     {
+        //노션 정리 필요
         "id": 15,
         "company": "프리미엄",
         "type": "마그네슘",
         "name": "마그네슘 400",
         "image": "images/drug-15.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "식품으로는 견과류, 해산물, 유제품, 초콜릿 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-nuts.png",
+        "modalImage2": "images/food-seafood.png",
+        "modalImage3": "images/food-dailyproduct.png",
+        "modalImage4": "images/food-chocolate.png"
     },
     {
         "id": 16,
@@ -125,7 +210,13 @@ const DrugData = [
         "type": "마그네슘",
         "name": "마그네슘",
         "image": "images/drug-16.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "식품으로는 견과류, 해산물, 유제품, 초콜릿 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-nuts.png",
+        "modalImage2": "images/food-seafood.png",
+        "modalImage3": "images/food-dailyproduct.png",
+        "modalImage4": "images/food-chocolate.png"
     },
     {
         "id": 17,
@@ -133,7 +224,13 @@ const DrugData = [
         "type": "철분제",
         "name": "헤모퀸즈 철분제",
         "image": "images/drug-17.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "철분제는 철분 결핍에 의한 탈모 증상, 무기력증, 만성 출혈, 빈혈을 예방합니다. 또한 ADHD 과잉 행동 증상 개선, 호흡 곤란 문제, 피로 회복을 개선하는 데에도 효과적입니다. 식품으로는 고기, 닭고기, 콩, 생선 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-meat.png",
+        "modalImage2": "images/food-chicken.png",
+        "modalImage3": "images/food-bean.png",
+        "modalImage4": "images/food-fish.png"
     }, 
     {
         "id": 18,
@@ -141,15 +238,24 @@ const DrugData = [
         "type": "철분제",
         "name": "프리미엄 철분제",
         "image": "images/drug-18.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "철분제는 철분 결핍에 의한 탈모 증상, 무기력증, 만성 출혈, 빈혈을 예방합니다. 또한 ADHD 과잉 행동 증상 개선, 호흡 곤란 문제, 피로 회복을 개선하는 데에도 효과적입니다. 식품으로는 고기, 닭고기, 콩, 생선 등을 통해 섭취가 가능합니다.",
+        "modalImage1": "images/food-meat.png",
+        "modalImage2": "images/food-chicken.png",
+        "modalImage3": "images/food-bean.png",
+        "modalImage4": "images/food-fish.png"
     },
     {
+        // 식품 내용 및 사진 필요
         "id": 19,
         "company": "트루포뮬러",
         "type": "오메가3",
         "name": "제일메가 오메가3",
         "image": "images/drug-19.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "오메가3는 우리 몸에 꼭 필요하지만 자체적으로 생산되지 않는 필수 지방산으로서 불포화지방산의 한 종류입니다. 세포막을 구성하는 주요 성분이며, 염증을 억제하는 기능과 세포에 산소를 원활하게 해주는 역할도 합니다. 심혈관 질환을 예방할 뿐만 아니라 염증 감소에 효과적이라 천식이나 만성염증 완화에 좋으며, 우울증이나 치매 예방에도 도움을 줍니다. 오메가3 건강식품을 복용하는 것이 좋은 예는 심장에 좋은 고밀도 콜레스테롤의 수치가 낮은 경우, 등푸른생선에 알레르기가 있어 식품으로 섭취하지 못하는 경우, 혈관질환자와 흡연자 중에서 아스피린 복용이 어려운 경우, 생선 속 수은을 피해야 하는 임산부와 어린이, 중성지방 수치가 높은 경우, 혈당 수치가 높은 경우입니다."
     },
     {
         "id": 20,
@@ -157,7 +263,9 @@ const DrugData = [
         "type": "오메가3",
         "name": "오메가3",
         "image": "images/drug-20.png",
-        "explain": "약 설명"
+        "explain": "약 설명",
+
+        "modalExplain": "오메가3는 우리 몸에 꼭 필요하지만 자체적으로 생산되지 않는 필수 지방산으로서 불포화지방산의 한 종류입니다. 세포막을 구성하는 주요 성분이며, 염증을 억제하는 기능과 세포에 산소를 원활하게 해주는 역할도 합니다. 심혈관 질환을 예방할 뿐만 아니라 염증 감소에 효과적이라 천식이나 만성염증 완화에 좋으며, 우울증이나 치매 예방에도 도움을 줍니다. 오메가3 건강식품을 복용하는 것이 좋은 예는 심장에 좋은 고밀도 콜레스테롤의 수치가 낮은 경우, 등푸른생선에 알레르기가 있어 식품으로 섭취하지 못하는 경우, 혈관질환자와 흡연자 중에서 아스피린 복용이 어려운 경우, 생선 속 수은을 피해야 하는 임산부와 어린이, 중성지방 수치가 높은 경우, 혈당 수치가 높은 경우입니다."
     }
 ]
 
